ed83dd64781c647569a63e5a646970059753ea0a
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2023-02-04 21:29:08 -0800

    feat(landing): name for landing page

--- components/hero.tsx
+++ components/hero.tsx
@@ -33,8 +33,8 @@ export default function Hero() {
         {SITE_DESCRIPTION}
       </h2>
       <h1 className={cn(styleUtils.appear, styleUtils['appear-third'], styles.hero)}>
-        Welcome to the
-        <br className={styleUtils['show-on-desktop']} /> {BRAND_NAME} conference
+        Issue a Verifiable Credential
+        <br className={styleUtils['show-on-desktop']} /> with {BRAND_NAME} Bank
       </h1>
       <h2
         className={cn(
@@ -50,7 +50,7 @@ export default function Hero() {
         <p>{DATE}</p>
         <div className={styles['description-separator']} />
         <p>
-          <strong>Online</strong>
+          <strong>TBD</strong>
         </p>
       </div>
     </div>
